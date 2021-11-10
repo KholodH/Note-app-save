8fcc3404e7ab4b59eaf8d0e1731d390f25f91a2d
KholodH <91002535+KholodH@users.noreply.github.com>
2021-11-10 10:53:27 +0300

    Update DBHelper.kt

--- app/src/main/java/com/example/noteappsave/DBHelper.kt
+++ app/src/main/java/com/example/noteappsave/DBHelper.kt
@@ -6,23 +6,23 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHelper(context: Context): SQLiteOpenHelper(context,"details.db", null, 1) {
-    private val sqLiteDatabase: SQLiteDatabase = writableDatabase
+   // private val sqLiteDatabase: SQLiteDatabase = writableDatabase
 
     override fun onCreate(db: SQLiteDatabase?) {
-        if (db != null) {
-            db.execSQL("create table notes (pk INTEGER PRIMARY KEY AUTOINCREMENT, note text)")
-        }
+        db?.execSQL("create table notes (pk INTEGER PRIMARY KEY AUTOINCREMENT,noteid int,note text)")
     }
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        sqLiteDatabase!!.execSQL("DROP TABLE IF EXISTS notes")  // This removes the table if a new version is detected
-        onCreate(sqLiteDatabase)
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS notes")  // This removes the table if a new version is detected
+        onCreate(db)
     }
 
-    fun saveData(note: String): Long {
+    fun saveData(note: NoteModel): Long {
+        val db = this.writableDatabase
         val contentValues = ContentValues()
         // There is no need to pass in the pk because it is automatically generated
-        contentValues.put("Note", note)
-       var nt= sqLiteDatabase.insert("notes", null, contentValues)
+        contentValues.put("note",note.noteText)
+       val nt= db.insert("notes", null, contentValues)
+        db.close()
         return nt
     }
 
